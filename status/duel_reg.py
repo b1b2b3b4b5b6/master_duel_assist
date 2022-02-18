e646143fb736cc2287defa775f55434bc366cdc6
b1b2b3b4b5b6 <a1439458305@163.com>
2022-02-18 19:59:26 +0800

    bug fix

--- status/duel_reg.py
+++ status/duel_reg.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-02-23 11:08:45
-LastEditTime: 2022-02-17 23:31:22
+LastEditTime: 2022-02-18 08:58:17
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \dllink_assist\base_reg.py
@@ -10,6 +10,22 @@ import tool
 import logging
 from status.base_reg import STATUS_BASE
 from dict_recursive_update import recursive_update
+
+
+class STATUS_DUEL_FINDOPPONECTRETRY(STATUS_BASE):
+    def __init__(self):
+        super().__init__()
+
+        custom_dict = {
+            'STATUS_DUEL_MAIN1': tool.OperationClickOnImg('img/duel/find_opponent_retry.png')
+        }
+        recursive_update(self.transfer_dict, custom_dict)
+
+        self.staimg_list = [
+            tool.ProofImg('img/duel/find_opponent_retry.png')
+        ]
+
+        self.priority = STATUS_BASE.PRI_HIGH
 
 
 class STATUS_DUEL_NOTSELECT(STATUS_BASE):
